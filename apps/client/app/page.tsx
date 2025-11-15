56da7e1af8372a505e7a10c5cde11b3a9c284819
André Ponce <asponceg@gmail.com>
2025-11-15 15:53:58 -0600

    Integrating postgreSQL

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import { Button, Flex, Text } from "@radix-ui/themes";
+
 export default function HomePage() {
-  return <div>Hello world</div>;
+  return (
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes :)</Text>
+      <Button>Let's go</Button>
+    </Flex>
+  );
 }
